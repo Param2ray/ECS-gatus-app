1b26f737936d43b0adf5675c1852bb62520419c1
Param2ray <param_2ray@hotmail.co.uk>
2026-01-25 20:38:00 -0500

    updated bootstrap code

--- terraform-bootstrap/variables.tf
+++ terraform-bootstrap/variables.tf
@@ -4,9 +4,15 @@ variable "aws_region" {
 }
 
 variable "github_repo" {
-  type        = string
   description = "OWNER/REPO, e.g. Param2ray/ecs-production-healthcheck-service"
+  type        = string
+
+  validation {
+    condition     = can(regex(".+/.+", var.github_repo))
+    error_message = "github_repo must be in OWNER/REPO format (example: Param2ray/ecs-production-healthcheck-service)."
+  }
 }
+
 
 variable "state_bucket" {
   type        = string
